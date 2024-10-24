571b3707c507225e06271cadafd69c938b307550
”Kyle” <2565490m@student.gla.ac.uk>
2024-10-24 16:22:15 +0100

    Initial Commit to test packages

---
+++ src/main/java/com/simulator/Simulation.java
@@ -0,0 +1,13 @@
+package com.simulator;
+
+/**
+ * Hello world!
+ *
+ */
+public class Simulation
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
